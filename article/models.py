bed51d1b19dd67c833b4205fa94816c4bd135fe9
yjj1519691288@163.com <yjj1519691288@163.com>
2021-04-28 10:04:38 +0800

    complete some function of article

--- article/models.py
+++ article/models.py
@@ -19,6 +19,8 @@ class ArticlePost(models.Model):
     created = models.DateTimeField(default=timezone.now)
     # 文章更新时间。参数 auto_now=True 指定每次数据更新时自动写入当前时间
     updated = models.DateTimeField(auto_now=True)
+    # 文章浏览量
+    total_views = models.PositiveIntegerField(default=0)
 
     class Meta:
         db_table = 'article'
--- article/views.py
+++ article/views.py
@@ -8,14 +8,62 @@ from .forms import ArticlePostForm
 from django.contrib.auth.models import User
 # 引入验证登录的装饰器
 from django.contrib.auth.decorators import login_required
+# 引入分页模块
+from django.core.paginator import Paginator
+# 引入Q对象
+from django.db.models import Q
 
 
 # Create your views here.
 def article_list(request):
-    # 取出所有博客文章
-    articles = ArticlePost.objects.all()
-    # 需要传递给模板（templates）的对象
-    context = {'articles': articles}
+    # # 取出所有博客文章
+    # articles = ArticlePost.objects.all()
+    # article_list = ArticlePost.objects.all()
+    # # 每页显示一篇文章
+    # paginator = Paginator(article_list, 3)
+    # # 取出url中的页码
+    # '''在GET请求中，在url的末尾附上?key=value的键值对，视图中就可以通过request.GET.get('key')来查询value的值'''
+    # page = request.GET.get('page')
+    # # 将导航对象相应的页码内容返回给 articles
+    # articles = paginator.get_page(page)
+    # # 需要传递给模板（templates）的对象
+    # context = {'articles': articles}
+    # 根据GET请求中查询条件返回不同排序的对象数组
+    search = request.GET.get('search')
+    order = request.GET.get('order')
+    # 用户搜索逻辑
+    if search:
+        if order == 'total_views':
+            # 用Q对象，进行联合搜索
+            article_list = ArticlePost.objects.filter(
+                Q(title__icontains=search) |
+                Q(body__icontains=search)
+            ).order_by('-total_views')
+        else:
+            article_list = ArticlePost.objects.filter(
+                Q(title__icontains=search) |
+                Q(body__icontains=search)
+            )
+    # TODO:更加复杂、深度定制的搜索.借助第三方模块，如Haystack
+    else:
+        # 将 search 参数重置为空
+        search = ''
+        if order == 'total_views':
+            article_list = ArticlePost.objects.all().order_by('-total_views')
+        else:
+            article_list = ArticlePost.objects.all()
+    # if request.GET.get('order') == 'total_views':
+    #     article_list = ArticlePost.objects.all().order_by('-total_views')
+    #     order = 'total_views'
+    # else:
+    #     article_list = ArticlePost.objects.all()
+    #     order = 'normal'
+
+    paginator = Paginator(article_list, 3)
+    page = request.GET.get('page')
+    articles = paginator.get_page(page)
+
+    context = {'articles': articles, 'order': order, 'search': search}
     # render函数：载入模板，并返回context对象
     return render(request, 'list.html', context)
 
@@ -24,16 +72,33 @@ def article_list(request):
 def article_detail(request, id):
     # 取出相应的文章
     article = ArticlePost.objects.get(id=id)
+    # 浏览量+1
+    article.total_views += 1
+    article.save(update_fields=['total_views'])  # update_fields=[]指定了数据库只更新total_views字段
     # 将markdown语法渲染成html样式
-    article.body = markdown.markdown(article.body, extensions=[
-        # 包含 缩写、表格等常用扩展
-        'markdown.extensions.extra',
-        # 语法高亮扩展
-        'markdown.extensions.codehilite',
-    ])
+    # article.body = markdown.markdown(article.body, extensions=[
+    #     # 包含 缩写、表格等常用扩展
+    #     'markdown.extensions.extra',
+    #     # 语法高亮扩展
+    #     'markdown.extensions.codehilite',
+    #     # 目录扩展
+    #     'markdown.extensions.TOC'
+    # ])
+    md = markdown.Markdown(
+        extensions=[
+            # 包含 缩写、表格等常用扩展
+            'markdown.extensions.extra',
+            # 语法高亮扩展
+            'markdown.extensions.codehilite',
+            # 目录扩展
+            'markdown.extensions.toc'
+        ]
+    )
+    # 将 article.body 中的 Markdown 文本解析成 HTML 文本
+    article.body = md.convert(article.body)
 
     # 需要传递给模板的对象
-    context = {'article': article}
+    context = {'article': article, 'toc': md.toc}
     # 载入模板，并返回context对象
     return render(request, 'detail.html', context)
 
@@ -67,7 +132,7 @@ def article_create(request):
         return render(request, 'create.html')
 
 
-# TODO:article_delete&article_update need verification
+# TODO:article_delete&article_update(ok) need verification
 # 删除文章
 def article_delete(request, id):
     # 根据id获取需要删除的文章
@@ -89,9 +154,13 @@ def article_safe_delete(request, id):
 
 
 # 更新文章
+@login_required(login_url='urserprofilr/login/')  # 过滤未登录的用户,提醒用户登录
 def article_update(request, id):
     # 获取需要修改的具体文章对象
     article = ArticlePost.objects.get(id=id)
+    # 过滤非作者的用户
+    if request.user != article.author:
+        return HttpResponse('抱歉，你无权修改这篇文章。')
     # 判断用户是否为POST提交表单数据
     if request.method == 'POST':
         # 将提交的数据赋值到表单实例中
