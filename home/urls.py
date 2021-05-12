4a756c7446d26a0641b1f2f9f5623f2e4be875b8
Ryan QR <13981709113@163.com>
2021-05-12 15:23:06 +0800

    Added other templates for the home page

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,8 @@ from home import views
 
 urlpatterns = [
     url(r'^index/',views.index),
+    url(r'^about/',views.about),
+    url(r'^contact',views.contact),
+    url(r'^gallery',views.gallery),
+    url(r'^single_post',views.single_post),
 ]
